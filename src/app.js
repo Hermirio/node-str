8de54b299a6f7b810d4fa08791e73777dc746ec4
hermirio <wandersonhermirio@gmail.com>
2019-02-03 19:08:00 -0300

    Criamos um arquivo com as configurações da rotas da aplicação

--- src/app.js
+++ src/app.js
@@ -6,35 +6,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+//carrega as Rotas
+const index = require('./routes/index');
+const product = require('./routes/product')
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ 
 	extended: false 
 }));
 
-const route = router.get('/', (req, res, next) => {
-	res.status(200).send({
-		title: "Node Store API",
-		version: "0.0.1"
-	});
-});
-
-const create = router.post('/', (req, res, next) => {
-	res.status(201).send(req.body);
-});
-const put = router.put('/:id', (req, res, next) => {
-	let id = req.params.id;
-	res.status(200).send({
-		id: id,
-		item: req.body
-	});
-});
-const del = router.delete('/', (req, res, next) => {
-	res.status(200).send(req.body);
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', index);
+app.use('/products', product);
 
 module.exports = app;
