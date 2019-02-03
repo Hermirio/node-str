96d46c17aa188f34b9e5ba00fa86d592743751f8
hermirio <wandersonhermirio@gmail.com>
2019-02-03 18:12:47 -0300

    Nessa versão criamos os metodo create e put, e acrescentamos a lib body-parser

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ 
 	extended: false 
 }));
