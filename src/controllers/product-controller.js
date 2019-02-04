14161f7cbb44cd34903ea6863c90b063c6a78126
hermirio <wandersonhermirio@gmail.com>
2019-02-04 16:37:59 -0300

    Implementamos o metodo delete na API

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -5,51 +5,51 @@ const Product = mongoose.model('Product');
 
 exports.get = (req, res, next) => {
 	Product
-	.find({
-		active: true
-	}, 'title price slug')
-	.then(data => {
-		res.status(200).send(data);
-	}).catch(e => {
-		res.status(400).send(e);
-	});
+		.find({
+			active: true
+		}, 'title price slug')
+		.then(data => {
+			res.status(200).send(data);
+		}).catch(e => {
+			res.status(400).send(e);
+		});
 };
 
 exports.getBySlug = (req, res, next) => {
 	Product
-	.findOne({
-		slug: req.params.slug,
-		active: true
-	}, 'title description price slug tags')
-	.then(data => {
-		res.status(200).send(data);
-	}).catch(e => {
-		res.status(400).send(e);
-	});
+		.findOne({
+			slug: req.params.slug,
+			active: true
+		}, 'title description price slug tags')
+		.then(data => {
+			res.status(200).send(data);
+		}).catch(e => {
+			res.status(400).send(e);
+		});
 };
 
 exports.getById = (req, res, next) => {
 	Product
-	.findById(req.params.id)
-	.then(data => {
-		res.status(200).send(data);
-	}).catch(e => {
-		res.status(400).send(e);
-	});
+		.findById(req.params.id)
+		.then(data => {
+			res.status(200).send(data);
+		}).catch(e => {
+			res.status(400).send(e);
+		});
 };
 
 
 exports.getByTag = (req, res, next) => {
 	Product
-	.find({
-		tags: req.params.tag,
-		active: true
-	}, 'title description price slug tags')
-	.then(data => {
-		res.status(200).send(data);
-	}).catch(e => {
-		res.status(400).send(e);
-	});
+		.find({
+			tags: req.params.tag,
+			active: true
+		}, 'title description price slug tags')
+		.then(data => {
+			res.status(200).send(data);
+		}).catch(e => {
+			res.status(400).send(e);
+		});
 };
 
 exports.post = (req, res, next) => {
@@ -67,13 +67,34 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = ('/:id', (req, res, next) => {
-	let id = req.params.id;
-	res.status(200).send({
-		id: id,
-		item: req.body
-	});
+	Product
+		.findByIdAndUpdate(req.params.id, {
+			$set: {
+				title: req.body.title,
+				description: req.body.description,
+				price: req.body.price,
+				slug: req.body.slug,
+				tags: req.body.tags
+			}
+		}).then(x => {
+			res.status(200).send({ message: 'Produto atualizado com sucesso!'});
+		}).catch(e => {
+			res.status(400).send({ 
+				message: 'Falha na atualização',
+				data: e
+			});
+		});
 });
 
 exports.delete = ('/', (req, res, next) => {
-	res.status(200).send(req.body);
+	Product
+		.findOneAndRemove(req.body.id)
+		.then(x => {
+			res.status(200).send({ message: 'Produto Removido com sucesso!'});
+		}).catch(e => {
+			res.status(400).send({ 
+				message: 'Falha ao Remover',
+				data: e
+			})
+		});
 });
